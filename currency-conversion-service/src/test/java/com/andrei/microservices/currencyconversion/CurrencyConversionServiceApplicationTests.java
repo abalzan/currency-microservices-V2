6413a206b5bba08524bc0f6bbc4adbeef972cbfb
Andrei Balzan <andreibalzan@gmail.com>
2021-02-12 09:14:38 +0000

    initial commit currency conversion service

---
+++ currency-conversion-service/src/test/java/com/andrei/microservices/currencyconversion/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.microservices.currencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
