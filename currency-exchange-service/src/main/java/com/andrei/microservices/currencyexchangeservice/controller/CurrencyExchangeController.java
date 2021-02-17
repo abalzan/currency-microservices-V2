8c25530fb1b14a805b7815bbe18fe6edd0e9a7e7
Andrei Balzan <andreibalzan@gmail.com>
2021-02-17 12:11:45 +0000

    add zipkin for log tracing

--- currency-exchange-service/src/main/java/com/andrei/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/andrei/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,11 +3,13 @@ package com.andrei.microservices.currencyexchangeservice.controller;
 import com.andrei.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.andrei.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CurrencyExchangeController {
@@ -18,6 +20,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to ) {
 
+        log.info("retrieveExchangeValue called with {} to {}", from, to);
         final CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to).orElseThrow();
         currencyExchange.setEnvironmentPort(environment.getProperty("local.server.port"));
 
